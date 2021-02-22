d59968a2fd6081eee88d9bacd422b9922dc0e396
didierganthier <didierganthierperan@gmail.com>
2021-02-22 11:38:40 -0500

    navigation to HomePage.dart

--- lib/MySplashPage.dart
+++ lib/MySplashPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jarvis_object_detector_app/HomePage.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 class MySplashPage extends StatefulWidget {
@@ -11,6 +12,7 @@ class _MySplashPageState extends State<MySplashPage> {
   Widget build(BuildContext context) {
     return SplashScreen(
       seconds: 5,
+      navigateAfterSeconds: HomePage(),
       imageBackground: Image.asset("assets/back.jpg").image,
       useLoader: true,
       loaderColor: Colors.pink,
