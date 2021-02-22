707828361f8cb9491b5c73b911ff240337601203
didierganthier <didierganthierperan@gmail.com>
2021-02-22 12:08:58 -0500

    camera opening on click

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -39,6 +39,56 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/jarvis.jpg")
+              )
+            ),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Center(
+                      child: Container(
+                        color: Colors.black,
+                        height: 360,
+                        width: 360,
+                        child: Image.asset("assets/camera.jpg"),
+                      ),
+                    ),
+                    Center(
+                      child: FlatButton(
+                        onPressed: () {
+                          initCamera();
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(top: 35),
+                          height: 270,
+                          width: 360,
+                          child: imgCamera == null?
+                          Container(
+                            height: 270,
+                            width: 360,
+                            child: Icon(Icons.photo_camera, color: Colors.blueAccent, size: 40,),
+                          ) :
+                          AspectRatio(
+                            aspectRatio: cameraController.value.aspectRatio,
+                            child: CameraPreview(cameraController),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
