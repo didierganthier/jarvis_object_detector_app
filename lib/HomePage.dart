8b6b553bd2f7fe86f1c5997140416a5d84617f43
didierganthier <didierganthierperan@gmail.com>
2021-02-22 11:48:20 -0500

    camera setup in HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,6 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'main.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,6 +8,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  bool isWorking = false;
+  String result = "";
+  CameraController cameraController;
+  CameraImage imgCamera;
+
+  initCamera()
+  {
+    cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+    cameraController.initialize().then((value)
+    {
+      if(!mounted)
+      {
+        return;
+      }
+
+      setState(() {
+        cameraController.startImageStream((image) =>
+        {
+          if(!isWorking)
+          {
+            isWorking = true,
+            imgCamera = image,
+          }
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
